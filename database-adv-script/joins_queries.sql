5cbb403bd219d9838b4239fbcdd31df64e224a3e
nameisunknown <orangekapital@gmail.com>
2025-09-03 21:28:52 +0000

    add: a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,25 +1,56 @@
 SELECT
-b.booking_id,
-b.user_id,
-b.property_id,
-b.start_date,
-b.end_date,
-b.total_amount,
-b.status,
-u.user_id,
-u.first_name,
-u.last_name
+	b.booking_id,
+	b.user_id,
+	b.property_id,
+	b.start_date,
+	b.end_date,
+	b.total_amount,
+	b.status,
+	u.user_id,
+	u.first_name,
+	u.last_name
 
 FROM booking AS b
 INNER JOIN Users u ON b.booking_id = u.user_id;
 
 SELECT 
-p.property_id,
-p.title,
-r.review_id,
-r.rating,
-r.comment,
-r.created_at
+	p.property_id,
+	p.title,
+	r.review_id,
+	r.rating,
+	r.comment,
+	r.created_at
 
 FROM Property p
 LEFT JOIN Review r ON p.property_id = r.review_id;
+
+SELECT 
+	u.user_id,
+	u.first_name,
+	u.last_name,
+	u.email,
+	b.booking_id,
+	b.property_id,
+	b.start_date,
+	b.end_date,
+	b.total_amount,
+	b.status
+FROM Users u
+LEFT JOIN Booking b ON u.user_id = b.user_id
+
+UNION
+
+SELECT 
+	u.user_id,
+	u.first_name,
+	u.last_name,
+	u.email,
+	b.booking_id,
+	b.property_id,
+	b.start_date,
+	b.end_date,
+	b.total_amount,
+	b.status
+FROM Users u
+RIGHT JOIN Booking b ON u.user_id = b.user_id;
+
